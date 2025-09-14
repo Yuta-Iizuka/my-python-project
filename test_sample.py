1c5c0b63ae71a234ec8de4e67fe152c85a034f76
yuta-iizuka <zunzun2682@gmail.com>
2025-09-14 21:34:48 +0900

    test_success_and_fail

--- test_sample.py
+++ test_sample.py
@@ -4,6 +4,10 @@ def add(a, b):
 def test_add():
     assert add(2, 3) == 5
 
+# 成功テスト
+def test_success_example():
+    assert 2 * 3 == 6
+
+# 失敗テスト（前回の失敗テスト）
 def test_fail_example():
-    assert 1 + 1 == 3  # わざと間違ったテスト
-    
+    assert 1 + 1 == 3
